2bf34313eb6d54f71c844d73b499b9325ec26e89
Fermin Sandoval <ferminsandoval950@hotmail.com>
2019-10-03 21:27:48 -0600

    test travisCI

---
+++ scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Running deploy script"
+set -ev
+if [ $# -ne 3 ]
+then
+    echo "Invalid number of arguments passed, required 3, found $#"
+    exit 1
+fi
+
+TAG=$1
+DOCKER_USERNAME=$2
+DOCKER_PASSWORD=$3
+
+echo "TAG: $TAG"
+docker build -f ./test/testWebAppReact/Dockerfile -t "furman9596/test:$TAG"  .
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push furman9596/test:$TAG
