d89162ae3a021598a2e4c54d28664ed19fd9298a
Fermin Sandoval <ferminsandoval950@hotmail.com>
2019-10-04 00:09:37 -0600

    should be able eto build for arm and amd64

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,9 +10,37 @@ fi
 TAG=$1
 DOCKER_USERNAME=$2
 DOCKER_PASSWORD=$3
+DOCKERFILE_LOCATION="./testWebAppReact/Dockerfile"
 
-echo "TAG: $TAG"
-docker build -f ./testWebAppReact/Dockerfile -t "furman9596/test:$TAG" .
-echo "$DOCKER_PASSWORD"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push furman9596/test:$TAG
+
+buildctl build --frontend dockerfile.v0 \
+            --local dockerfile=. \
+            --local context=. \
+            --exporter image \
+            --exporter-opt name=docker.io/furman9596/test:${TRAVIS_TAG}-amd64 \
+            --exporter-opt push=true \
+            --frontend-opt platform=linux/amd64 \
+            --frontend-opt filename=./${DOCKERFILE_LOCATION}
+
+buildctl build --frontend dockerfile.v0 \
+            --local dockerfile=. \
+            --local context=. \
+            --exporter image \
+            --exporter-opt name=docker.io/furman9596/test:${TRAVIS_TAG}-arm \
+            --exporter-opt push=true \
+            --frontend-opt platform=linux/armhf \
+            --frontend-opt filename=./${DOCKERFILE_LOCATION}
+
+
+export DOCKER_CLI_EXPERIMENTAL=enabled
+
+
+docker manifest create furman9596/test:${TRAVIS_TAG} \
+            furman9596/test:${TRAVIS_TAG}-amd64 \
+            furman9596/test:${TRAVIS_TAG}-arm
+
+docker manifest annotate furman9596/test:${TRAVIS_TAG} furman9596/test:${TRAVIS_TAG}-arm --arch arm
+docker manifest annotate furman9596/test:${TRAVIS_TAG} furman9596/test:${TRAVIS_TAG}-amd64 --arch amd64
+
+docker manifest push furman9596/test:${TRAVIS_TAG}
